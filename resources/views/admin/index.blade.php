e9d29fb4134be194ef93f7c285d0e3cb0e66bb1f
Yigithan Üstün <xayekk@gmail.com>
2025-02-03 00:07:42 +0300

    Integrating Admin Template In Laravel Project

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,21 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admin Dashboard') }}
-        </h2>
-    </x-slot>
-
+<html>
+  <head> 
+    @include('admin.css')
+  </head>
+  <body>
+    @include('admin.header')
     
-</x-app-layout>
+    @include('admin.sidebar')
+    
+      @include('admin.body')
+        
+        @include('admin.footer')
 
-</body>
+  </body>
 </html>
