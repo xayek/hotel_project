afd56a5b41542bf5e5bdc1e435c9ff4180158ab6
Yigithan Üstün <xayekk@gmail.com>
2025-02-04 22:19:29 +0300

    Make Admin Middleware

--- routes/web.php
+++ routes/web.php
@@ -4,44 +4,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 
+// Ana sayfa ve misafir işlemleri için rotalar (middleware yok)
 route::get('/', [AdminController::class, 'home']);
-
-route::get('/home', [AdminController::class, 'index'])->name('home');
-
-route::get('/create_room', [AdminController::class, 'create_room']);
-
-route::post('/add_room', [AdminController::class, 'add_room']);
-
-route::get('/view_room', [AdminController::class, 'view_room']);
-
-route::get('/room_delete/{id}', [AdminController::class, 'room_delete']);
-
-route::get('/room_update/{id}', [AdminController::class, 'room_update']);
-
-route::post('/edit_room/{id}', [AdminController::class, 'edit_room']);
-
 route::get('/room_details/{id}', [HomeController::class, 'room_details']);
-
 route::post('/add_booking/{id}', [HomeController::class, 'add_booking']);
-
-route::get('/bookings', [AdminController::class, 'bookings']);
-
-route::get('/delete_booking/{id}', [AdminController::class, 'delete_booking']);
-
-route::get('/approve_book/{id}', [AdminController::class, 'approve_book']);
-
-route::get('/reject_book/{id}', [AdminController::class, 'reject_book']);
-
-route::get('/view_gallary', [AdminController::class, 'view_gallary']);
-
-route::post('/upload_gallary', [AdminController::class, 'upload_gallary']);
-
-route::get('/delete_gallary/{id}', [AdminController::class, 'delete_gallary']);
-
 route::post('/contact', [HomeController::class, 'contact']);
 
-route::get('/all_messages', [AdminController::class, 'all_messages']);
-
-route::get('/send_mail/{id}', [AdminController::class, 'send_mail']);
-
-route::post('/mail/{id}', [AdminController::class, 'mail']);
+// Admin işlemleri için middleware'li rotalar
+route::get('/home', [AdminController::class, 'index'])->middleware(['auth', 'admin'])->name('home');
+route::get('/create_room', [AdminController::class, 'create_room'])->middleware(['auth', 'admin']);
+route::post('/add_room', [AdminController::class, 'add_room'])->middleware(['auth', 'admin']);
+route::get('/view_room', [AdminController::class, 'view_room'])->middleware(['auth', 'admin']);
+route::get('/room_delete/{id}', [AdminController::class, 'room_delete'])->middleware(['auth', 'admin']);
+route::get('/room_update/{id}', [AdminController::class, 'room_update'])->middleware(['auth', 'admin']);
+route::post('/edit_room/{id}', [AdminController::class, 'edit_room'])->middleware(['auth', 'admin']);
+route::get('/bookings', [AdminController::class, 'bookings'])->middleware(['auth', 'admin']);
+route::get('/delete_booking/{id}', [AdminController::class, 'delete_booking'])->middleware(['auth', 'admin']);
+route::get('/approve_book/{id}', [AdminController::class, 'approve_book'])->middleware(['auth', 'admin']);
+route::get('/reject_book/{id}', [AdminController::class, 'reject_book'])->middleware(['auth', 'admin']);
+route::get('/view_gallary', [AdminController::class, 'view_gallary'])->middleware(['auth', 'admin']);
+route::post('/upload_gallary', [AdminController::class, 'upload_gallary'])->middleware(['auth', 'admin']);
+route::get('/delete_gallary/{id}', [AdminController::class, 'delete_gallary'])->middleware(['auth', 'admin']);
+route::get('/all_messages', [AdminController::class, 'all_messages'])->middleware(['auth', 'admin']);
+route::get('/send_mail/{id}', [AdminController::class, 'send_mail'])->middleware(['auth', 'admin']);
+route::post('/mail/{id}', [AdminController::class, 'mail'])->middleware(['auth', 'admin']);
